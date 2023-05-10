8a35816152bd29646131c1623c38dd72aef58a98
nascimentolds <nascimentolds@hotmail.com>
2023-05-10 16:16:55 -0300

    mmc

--- ADS/1-PERIODO/LP/20230504/dic05.py
+++ ADS/1-PERIODO/LP/20230504/dic05.py
@@ -1,5 +1,18 @@
 # 5. Escreva um programa que determine o menor número inteiro positivo
-# que pode ser dividido por todos os números de 1 a 15 sem deixar
+# que pode ser dividido por todos os números de 1 a 20 sem deixar
 # resto.
+ 
+def mmc(a, b):
+    """
+    Retorna o mínimo múltiplo comum entre dois números.
+    """
+    m = a * b
+    while a % b != 0:
+        a, b = b, a % b
+    return m // b
 
+mmc_total = 1
+for i in range(1, 21):
+    mmc_total = mmc(mmc_total, i)
 
+print(mmc_total)
