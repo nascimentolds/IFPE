787d40a161aabfd0b43ddeddd7a9795cf8ac80ef
Marcelo Nascimento <47907106+nascimentolds@users.noreply.github.com>
2023-05-26 21:29:06 -0300

    Aula 25-05

--- ADS/1-PERIODO/LP/20230525/rec05.py
+++ ADS/1-PERIODO/LP/20230525/rec05.py
@@ -1,13 +1,1 @@
-# 6. Calcular o enésimo termo da sequência de números de Tribonacci. 
-#  - A sequência de Tribonacci é uma extensão da sequência de Fibonacci, 
-#    em que cada termo é a soma dos três termos anteriores. 
-#    A sequência começa com 0, 1, 1 e, a partir daí, cada termo subsequente 
-#    é a soma dos três termos anteriores. 
-
-
-def fib(n):
-    if n == 0 or n == 1: return 0
-    elif n == 2 or n == 3: return 1
-    else: return fib(n-1) + fib(n-2) + fib(n-3)
-
-print(fib(18))
+# 5. Crie uma função recursiva para calcular a média dos elementos em uma lista.
---
+++ ADS/1-PERIODO/LP/20230525/rec07.py
@@ -0,0 +1,6 @@
+# 7. Crie uma função recursiva para verificar se um número é primo.
+
+def primo(n):
+    return n
+
+print(primo(5))
