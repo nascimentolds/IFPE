40e8a1851b7d1c20ce701898bd5cc95f854a97ec
nascimentolds <nascimentolds@hotmail.com>
2023-06-02 13:07:30 -0300

    primos

--- ADS/1-PERIODO/LP/20230518/rec05.py
+++ ADS/1-PERIODO/LP/20230518/rec05.py
@@ -2,11 +2,11 @@
 # inverta a posição dos seus elementos.
 
 def inverter(lista, n):
-  if n <= 1:
-      return lista
-  else:
-      lista[0], lista[n-1] = lista[n-1], lista[0]
-      return [lista[0]] + inverter(lista[1:n-1], n-2) + [lista[n-1]]
+    if n <= 1:
+        return lista
+    else:
+        lista[0], lista[-1] = lista[-1], lista[0]
+        return [lista[0]] + inverter(lista[1:n-1], n-2) + [lista[n-1]]
   
 lista = [1, 2, 3, 4, 5]
 print(inverter(lista, len(lista)))
--- ADS/1-PERIODO/LP/20230525/rec07.py
+++ ADS/1-PERIODO/LP/20230525/rec07.py
@@ -1,6 +1,14 @@
 # 7. Crie uma função recursiva para verificar se um número é primo.
 
-def primo(n):
-    return n
+def is_prime(n, divisor=2):
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    if n % divisor == 0:
+        return False
+    if divisor * divisor > n:
+        return True
+    return is_prime(n, divisor + 1)
 
-print(primo(5))
+print(is_prime(0))
